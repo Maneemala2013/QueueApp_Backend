462f11c6157c9b7f18931c4735f0456a12be5ace
nsangsiri <prabataroh@gmail.com>
2024-01-14 18:32:42 +0800

    edit datetime to char in appointment

--- api/serializers.py
+++ api/serializers.py
@@ -1,77 +1,73 @@
-from . import models
 from rest_framework import serializers
 from rest_framework.fields import CharField
 
-class ShopSerializer(serializers.HyperlinkedModelSerializer):
+from . import models
 
-	shop_name = CharField(source="title", required=True)
-	
-	class Meta:
-		model = models.Shop
-		fields = (
-			'shop_name',
-			'shop_detail',
-			'category',
-			'phone_number',
-			'working_hour',
-			'payment',
-			'ig_display',
-			'location',
-			'farness',
-			'price_range',
-			'rating',
-			'review_num',
-			'fb',
-			'profile_image_url',
-			'appointment_set',
-			'service_set',
-			'available_time_slot'
-		)
+
+class ShopSerializer(serializers.HyperlinkedModelSerializer):
+    shop_name = CharField(source="title", required=True)
+
+    class Meta:
+        model = models.Shop
+        fields = (
+            "shop_name",
+            "shop_detail",
+            "category",
+            "phone_number",
+            "working_hour",
+            "payment",
+            "ig_display",
+            "location",
+            "farness",
+            "price_range",
+            "rating",
+            "review_num",
+            "fb",
+            "profile_image_url",
+            "appointment_set",
+            "service_set",
+            "available_time_slot",
+        )
+
 
 class ServiceSerializer(serializers.ModelSerializer):
+    service_name = CharField(source="title", required=True)
+    service_detail = CharField(source="description", required=False)
 
-	service_name = CharField(source="title", required=True)
-	service_detail = CharField(source="description", required=False)
-	
-	class Meta:
-		model = models.service
-		fields = (
-			'service_name',
-			'service_detail',
-			'duration',
-			'price',
-			'discountedPrice',
-			'shop'
-		)
+    class Meta:
+        model = models.service
+        fields = (
+            "service_name",
+            "service_detail",
+            "duration",
+            "price",
+            "discountedPrice",
+            "shop",
+        )
+
 
 class UserSerializer(serializers.ModelSerializer):
+    User_name = CharField(source="title", required=True)
 
-	User_name = CharField(source="title", required=True)
-	
-	class Meta:
-		model = models.User
-		fields = (
-			'User_name',
-			'phone_number',
-		)
-
+    class Meta:
+        model = models.User
+        fields = (
+            "User_name",
+            "phone_number",
+            "appointment_set",
+        )
+        
 class AppointmentSerializer(serializers.ModelSerializer):
+    service_name = CharField(source="title", required=True)
+    service_detail = CharField(source="description", required=True)
 
-	service_name = CharField(source="title", required=True)
-	service_detail = CharField(source="description", required=True)
-	
-	class Meta:
-		model = models.Appointment
-		fields = (
-			'service_name',
-			'service_detail',
-			'user',
-			'shop',
-			'date',
-			'start_time',
-			'end_time'
-		)
-	# def to_representation(self, instance):
-	# 	self.fields['user'] =  UserSerializer(read_only=True)
-	# 	self.fields['shop'] =  ShopSerializer(read_only=True)
-	# 	return super(AppointmentSerializer, self).to_representation(instance)
+    class Meta:
+        model = models.Appointment
+        fields = (
+            "service_name",
+            "service_detail",
+            "user",
+            "shop",
+            "start_time",
+            "end_time",
+        )
