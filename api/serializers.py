6da3f91339fc6020b367abb5064d4edb33c3e1bf
Maneemala2013 <s6007503@mwit.ac.th>
2024-01-14 17:48:20 +0800

    Edit serializer to show application/json for appointment class

--- api/serializers.py
+++ api/serializers.py
@@ -2,22 +2,6 @@ from . import models
 from rest_framework import serializers
 from rest_framework.fields import CharField
 
-class AppointmentSerializer(serializers.ModelSerializer):
-
-	service_name = CharField(source="title", required=True)
-	service_detail = CharField(source="description", required=True)
-	
-	class Meta:
-		model = models.Appointment
-		fields = (
-			'service_name',
-			'service_detail',
-			'user',
-			'shop',
-			'start_time',
-			'end_time'
-		)
-
 class ShopSerializer(serializers.HyperlinkedModelSerializer):
 
 	shop_name = CharField(source="title", required=True)
@@ -70,3 +54,24 @@ class UserSerializer(serializers.ModelSerializer):
 			'User_name',
 			'phone_number',
 		)
+
+class AppointmentSerializer(serializers.ModelSerializer):
+
+	service_name = CharField(source="title", required=True)
+	service_detail = CharField(source="description", required=True)
+	
+	class Meta:
+		model = models.Appointment
+		fields = (
+			'service_name',
+			'service_detail',
+			'user',
+			'shop',
+			'date',
+			'start_time',
+			'end_time'
+		)
+	# def to_representation(self, instance):
+	# 	self.fields['user'] =  UserSerializer(read_only=True)
+	# 	self.fields['shop'] =  ShopSerializer(read_only=True)
+	# 	return super(AppointmentSerializer, self).to_representation(instance)
